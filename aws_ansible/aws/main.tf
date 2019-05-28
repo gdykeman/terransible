a53b54e19f3d24be238c96c43deb2bfeee37858e
Gdykeman <gerald.dykeman@gmail.com>
2019-05-28 15:53:08 -0400

    update with new backend

--- aws_ansible/aws/main.tf
+++ aws_ansible/aws/main.tf
@@ -3,20 +3,21 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 }
-
-resource "aws_instance" "example" {
+resource "aws_instance" "terransible" {
   ami           = "${lookup(var.amis, var.region["primary"])}"
   instance_type = "${lookup(var.instance_type, var.region["primary"])}"
-
-  # provisioner "local-exec" {
-  #   command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  # }
 }
 
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
+  instance = "${aws_instance.terransible.id}"
 }
 
 output "ip" {
   value = "${aws_eip.ip.public_ip}"
+}
+
+terraform {
+  backend "s3" {
+    ##Partial Configuration##
+  }
 }
